8593bff0a6b2f1baeaefbad0a75c2bc6c4c7998c
Richard Cai <rcai@xogrp.com>
2015-01-05 17:40:12 +0800

    Splash problem on iOS

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,13 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
-        "id": "org.apache.cordova.splashscreen.SplashScreen",
-        "clobbers": [
-            "navigator.splashscreen"
-        ]
-    },
-    {
         "file": "plugins/org.apache.cordova.network-information/www/network.js",
         "id": "org.apache.cordova.network-information.network",
         "clobbers": [
@@ -21,14 +14,21 @@ module.exports = [
         "clobbers": [
             "Connection"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.splashscreen": "0.3.5",
     "org.apache.cordova.console": "0.2.12",
-    "org.apache.cordova.network-information": "0.2.14"
+    "org.apache.cordova.network-information": "0.2.14",
+    "org.apache.cordova.splashscreen": "0.3.5"
 }
 // BOTTOM OF METADATA
 });
